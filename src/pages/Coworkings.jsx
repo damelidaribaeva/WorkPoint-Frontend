81960c2a7e636ee669fd112908a21e621a92dcaa
Dameli Daribayeva <da_daribaeva@kbtu.kz>
2025-02-17 20:24:18 +0500

    Footer and Navbar was done

--- src/pages/Coworkings.jsx
+++ src/pages/Coworkings.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const Coworkings = () => {
+  const { types } = useParams();
+  console.log(types);
   return <div>Coworkings</div>;
 };
 
